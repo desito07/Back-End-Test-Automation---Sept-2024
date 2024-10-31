a89ac762051a1719eb2949c90d6af75c030f8a7a
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2024-10-31 21:11:25 +0000

    Add files via upload

--- test/sumNumbersTest.js
+++ test/sumNumbersTest.js
@@ -1,17 +1,30 @@
-import { expect } from 'chai';
-import { sum } from '../sumNumbers.js'; 
+const { expect } = require('chai');
+
+const { sum } = require('./sumNumbers.js');
+
 
 describe('Sum numbers', () => {
-    it('sums single number', () => {
-        expect(sum([1])).to.equal(1);
-    });
 
-    // Test overloading
-    it('sums multiple numbers', () => {
-        expect(sum([1,1])).to.equal(2);
-    });
+it('sums single number', () => {
 
-    it('sums different numbers', () => {
-        expect(sum([2,3,4])).to.equal(9);
-    });
+expect(sum([1])).to.equal(1);
+
+});
+
+
+// Test overloading
+
+it('sums multiple numbers', () => {
+
+expect(sum([1,1])).to.equal(2);
+
+});
+
+
+it('sums different numbers', () => {
+
+expect(sum([2,3,4])).to.equal(9);
+
+});
+
 });
